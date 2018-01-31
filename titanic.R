79b485b6d566a195958f0e22c66903ecc80c3af4
Everett Yee <eyee305@gmail.com>
2018-01-30 18:39:46 -0800

    Add files via upload

--- titanic.R
+++ titanic.R
@@ -81,3 +81,46 @@ Prediction <- predict(fit, test, type = "class")
 submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "decisiontreegonewrong.csv", row.names = FALSE) #Dropped to 74%
 #We overfit the model
+
+#Feature engineering
+train$Name[1]
+
+train <- read.csv("train.csv") #rereading dataset in for this part of the exercise, as column number does not match
+test <- read.csv("test.csv")
+
+test$Survived <- NA
+combi <- rbind(train,test) #combining training and testing sets
+
+combi$Name <- as.character(combi$Name) #setting factor back to string
+combi$Name[1]
+strsplit(combi$Name[1], split = '[,.]')[[1]][2] #[[1]] removes the [[1]], [2] chooses the 2nd index which is "Mr"
+combi$Title <- sapply(combi$Name, FUN = function(x) {strsplit(x, split = '[,.]')[[1]][2]})
+combi$Title <- sub(' ', '', combi$Title) #removing extra spaces
+table(combi$Title)
+
+combi$Title[combi$Title %in% c('Mme', 'Mlle')] <- 'Mlle' #combining madame and madmoiselle
+combi$Title[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+combi$Title[combi$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+combi$Title <- factor(combi$Title)
+
+combi$FamilySize <- combi$SibSp + combi$Parch + 1 #adding 1 is for their own existence
+
+combi$Surname <- sapply(combi$Name, FUN = function(x) {strsplit(x, split = '[,.]')[[1]][1]})
+combi$FamilyID <- paste(as.character(combi$FamilySize), combi$Surname, sep = "")
+combi$FamilyID[combi$FamilySize <= 2] <- 'Small'
+table(combi$FamilyID)
+
+famIDs <- data.frame(table(combi$FamilyID))
+famIDs <- famIDs[famIDs$Freq <= 2,]
+combi$FamilyID[combi$FamilyID %in% famIDs$Var1] <- 'Small'
+combi$FamilyID <- factor(combi$FamilyID)
+
+train <- combi[1:891,]
+test <- combi[892:1309,] #splitting data back into train and test
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID, data = train, method = "class")
+fancyRpartPlot(fit)
+
+Prediction <- predict(fit, test, type = "class")
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+write.csv(submit, file = "featureengineering.csv", row.names = FALSE) #Now at 79.4%
